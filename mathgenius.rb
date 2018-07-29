89b4d0c249e6616aa329a5a00d0c2bb717748113
curtischatch <curtis.chat.hk@gmail.com>
2018-07-29 15:53:15 -0400

    math genius works

---
+++ mathgenius.rb
@@ -0,0 +1,20 @@
+require_relative 'multilinguist'
+
+class Mathgenius < Multilinguist
+  def report(x)
+    total = 0
+    x.each do |number|
+      total += number
+    end
+    return "#{say_in_local_language('Skrrr the total is')} #{total}"
+  end
+end
+
+me = Mathgenius.new
+puts me.report []
+
+puts me.report([23,45,676,34,5778,4,23,5465])
+ me.travel_to('India')
+ puts me.report([6,3,6,68,455,4,467,57,4,534])
+ me.travel_to("Italy")
+puts me.report([324,245,6,343647,686545])
